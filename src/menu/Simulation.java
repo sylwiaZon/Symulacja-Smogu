7526f65ad55c52bc139e39c575b8fae6ff967332
ania wojcik <wojcikania12@wp.pl>
2019-05-20 10:37:11 +0200

    Sam algorytm Kiringa, nie uwzględnione warunki pogodowe

--- src/menu/Simulation.java
+++ src/menu/Simulation.java
@@ -1,5 +1,7 @@
 package menu;
 
+import smile.interpolation.KrigingInterpolation;
+
 enum AvaliableTraffic {
     LOW, HIGH, MEDIUM
 }
@@ -14,6 +16,10 @@ public class Simulation {
     String pmType;
     int duration;
     AvaliableTraffic traffic;
+    int[] sensorsCoordinatesX={1,2,3,4,5};
+    int[] sensorsCoordinatesY ={1,2,3,4,5};
+    double[] tempPM10 = {1,2,3,4,5};
+    private int size = 70;
     
     int getTemperature(){
         return temperature;
@@ -39,5 +45,51 @@ public class Simulation {
     AvaliableTraffic getTraffic(){
         return traffic;
     }
+    int[] getSensorsCoordinateX(){
+        return sensorsCoordinatesX;
+    }
+    int[] getSensorsCoordinateY(){
+        return sensorsCoordinatesY;
+    }
+    double[] getTempPM10(){
+        return tempPM10;
+    }
+
+
+    double[][] kriging(double[] weights) {
+
+        double[][] dataMatrix = new double[size][size];
+        double[][] coordinates = new double[sensorsCoordinatesX.length][2];
+
+        for(int i = 0; i< sensorsCoordinatesX.length; i++){
+            dataMatrix[sensorsCoordinatesX[i]][sensorsCoordinatesY[i]] = weights[i];
+            coordinates[i][0] = sensorsCoordinatesX[i];
+            coordinates[i][1] = sensorsCoordinatesY[i];
+        }
+
+        KrigingInterpolation kriging = new KrigingInterpolation(coordinates, weights);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(dataMatrix[i][j] == 0.0){
+                    dataMatrix[i][j]= kriging.interpolate(j, i);
+                }
+            }
+        }
+//        for (int i = 0; i < size; i++) {
+//            for (int j = 0; j <size; j++) {
+//                System.out.print(dataMatrix[i][j] + " ");
+//            }            System.out.println();
+//
+//        }
+        return dataMatrix;
+    }
+
+    double[][] propagation(double[][] dataMatrix){
+        double[][] finalData = new double[size][size];
+
+        return finalData;
+    }
+
+
 }
 
